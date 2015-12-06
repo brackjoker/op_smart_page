97296c80e07be60d1293370d4f4336a2d7882d46
brackjoker <brack.joker1@gmail.com>
2015-12-06 20:32:38 +0900

    rest body modifi

--- neutron.py
+++ neutron.py
@@ -8,6 +8,7 @@ from openstack_base import openstack_base
 class neutron:
 
     result_json = {}
+    server_all_info = {}
 
     @classmethod
     def getlist(self,ops_base_obj):
@@ -19,15 +20,4 @@ class neutron:
         resp, token = h.request(url, 'GET', headers=headers, body=body)
         token_body_byt = token
         toke_body = token_body_byt.decode(sys.stdin.encoding)
-        server_info = json.loads(toke_body)
-
-        for server_list in server_info['servers']:
-
-            result_json = {"server_name": str(server_list['name']),"nova_host": str(server_list['OS-EXT-SRV-ATTR:host']),"network":[]}
-            print("server name: ",str(server_list['name']))
-            print("nova host: ",str(server_list['OS-EXT-SRV-ATTR:host']))
-            i=0
-            for network_obj in server_list['addresses']:
-                result_json.update({"network":[{"address" : network_obj}]})
-                print(network_obj)
-                i = i + 1
+        self.server_all_info = json.loads(toke_body)
--- op_get_info/op_get_info.py
+++ op_get_info/op_get_info.py
@@ -3,7 +3,7 @@ import sys
 import json
 from openstack_base import openstack_base
 from nova import nova
-
+from neutron import neutron
 
 def getlist(req):
 
@@ -15,20 +15,20 @@ def getlist(req):
     opbase_obj.openstack_ip = '192.168.151.202'
     opbase_obj.get_token()
     opbase_obj.get_tenant_id()
-    nova_obj = nova
 
     if req.method == "POST":
         body_byt = req.body
         rest_obj = json.loads(body_byt.decode(sys.stdin.encoding))
 
-        if rest_obj['type'] == "nova":
+        if rest_obj['info_type'] == "nova":
+            nova_obj = nova
             nova_obj.getlist(opbase_obj)
             content = nova_obj.server_all_info
 
-
-        elif rest_obj['type'] == "neutron":
-            print("command")
-            content = {"result": "ok"}
+        elif rest_obj['info_type'] == "neutron":
+            neutron_obj = neutron
+            neutron_obj.getlist(opbase_obj)
+            content = neutron_obj.server_all_info
 
     return JsonResponse(content)
 
