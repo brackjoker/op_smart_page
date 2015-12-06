e8412931b1948315d04efb0e496f111e4b91aecb
brackjoker <brack.joker1@gmail.com>
2015-12-06 17:52:08 +0900

    bug fix

--- cmd_exe/cmd_operation.py
+++ cmd_exe/cmd_operation.py
@@ -15,7 +15,7 @@ def ops_cmd_exec(req):
         #if cmd_obj.chk_str_ascii(str(rest_obj['command'])):
 
         cmd_obj.cmd_str = str(rest_obj['command'])
-        cmd_obj.os_auth_url = 'http://192.168.151.176:35357/v2.0'
+        cmd_obj.os_auth_url = 'http://192.168.151.202:35357/v2.0'
         cmd_obj.os_username = 'admin'
         cmd_obj.os_password = 'admin'
         cmd_obj.os_tenant_name = 'admin'
