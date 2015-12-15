4fcdc59b66c3f847b3f73e8cd13715807edaa781
brackjoker <brack.joker1@gmail.com>
2015-12-15 17:28:57 +0900

    add massage remember function

---
+++ data_pool.py
@@ -0,0 +1,5 @@
+class data_pool:
+
+    massage1 = ""
+    massage2 = ""
+    instance_id = ""
--- yoshida_kun/artificial_intelligence_operations.py
+++ yoshida_kun/artificial_intelligence_operations.py
@@ -9,20 +9,15 @@ from make_params import make_params
 from text_classifier import text_classifier
 from cmd_op import comand_operation
 from file_function import file_function
+from data_pool import data_pool
+
+
+data_pool_obj = data_pool
 
 
 def exec_operation(req):
 
-    opbase_obj = openstack_base
-    opbase_obj.password = 'admin'
-    #opbase_obj.password = 'okinawa1940'
-    opbase_obj.username = 'admin'
-    opbase_obj.tenantname = 'admin'
 
-    #opbase_obj.openstack_ip = '192.168.1.29'
-    opbase_obj.openstack_ip = '192.168.249.197'
-    opbase_obj.get_token()
-    opbase_obj.get_tenant_id()
 
     target_image_name = ""
     network_id = ""
@@ -34,43 +29,63 @@ def exec_operation(req):
 
     if req.method == "POST":
         body_byt = req.body
+
         print "yoshida_kun request body:"+req.body
         rest_obj = json.loads(body_byt.decode(sys.stdin.encoding))
-        instance_id = rest_obj['instance_id']
-#        if str(instance_id) != "" :
-
-        file_function.make_tensor_file(str(rest_obj['massage1']))
-
-        cmd = text_classifier.exec_tensor()
-        #cmd = "rebuild"
-
-        if cmd == "none":
-            print "none operation"
-            content = {
-                    'massage': "",
-                    'err_massage': "operation is no command"
-                   }
-        elif cmd == "rebuild":
-
-            print "exec rebuild operation"
-            content = exec_rebuild(opbase_obj,instance_id)
+        if rest_obj['instance_id'] == "null" or rest_obj['instance_id'] == "":
+            data_pool_obj.massage1 = str(rest_obj['massage1'])
+            content = {}
         else:
-            print "exec command operation"
-            cmd_obj = comand_operation
-            cmd_obj.cmd_str = str(cmd)
-            cmd_obj.os_auth_url = 'http://192.168.249.197:35357/v2.0'
-            cmd_obj.os_username = 'admin'
-            cmd_obj.os_password = 'admin'
-            cmd_obj.os_tenant_name = 'admin'
-            cmd_obj.make_op_env_val()
-            cmd_obj.cmd_exec()
-#        else:
-    #        content = exec_rebuild(opbase_obj,instance_id)
-#            print 0
-            content = {
-                        'massage': cmd_obj.cmd_out_std,
-                        'err_massage': cmd_obj.cmd_err_std
+            data_pool_obj.instance_id = rest_obj['instance_id']
+            opbase_obj = openstack_base
+            opbase_obj.password = 'admin'
+            #opbase_obj.password = 'okinawa1940'
+            opbase_obj.username = 'admin'
+            opbase_obj.tenantname = 'admin'
+
+            #opbase_obj.openstack_ip = '192.168.1.29'
+            opbase_obj.openstack_ip = '192.168.249.197'
+            opbase_obj.get_token()
+            opbase_obj.get_tenant_id()
+
+
+            instance_id = data_pool_obj.instance_id
+    #        if str(instance_id) != "" :
+
+            file_function.make_tensor_file(str(data_pool_obj.massage1))
+
+            cmd = text_classifier.exec_tensor()
+            #cmd = "rebuild"
+
+            if cmd == "none":
+                print "none operation"
+                content = {
+                        'massage': "",
+                        'err_massage': "operation is no command"
                        }
+            elif cmd == "rebuild":
+
+                print "exec rebuild operation"
+                content = exec_rebuild(opbase_obj,instance_id)
+            else:
+                print "exec command operation"
+                cmd_obj = comand_operation
+                cmd_obj.cmd_str = str(cmd)
+                cmd_obj.os_auth_url = 'http://192.168.249.197:35357/v2.0'
+                cmd_obj.os_username = 'admin'
+                cmd_obj.os_password = 'admin'
+                cmd_obj.os_tenant_name = 'admin'
+                cmd_obj.make_op_env_val()
+                cmd_obj.cmd_exec()
+    #        else:
+        #        content = exec_rebuild(opbase_obj,instance_id)
+    #            print 0
+                content = {
+                            'massage': cmd_obj.cmd_out_std,
+                            'err_massage': cmd_obj.cmd_err_std
+                           }
+            data_pool_obj.massage1 = ""
+            data_pool_obj.instance_id = ""
 
     return JsonResponse(content)
 
